831cdea6500fca99eab095410e0e761db546e69e
Student <test@wp.pl>
2024-03-11 08:57:21 +0100

    added offline caching

--- service-worker.js
+++ service-worker.js
@@ -12,13 +12,31 @@ self.addEventListener('install', function (evt) {
         })
     );
 });
-self.addEventListener('fetch', function (evt) {
-    // console.log(event.request.url);
-    evt.respondWith(
-        // Firstly, send request..
-        fetch(evt.request).catch(function () {
-            // When request failed, return file from cache...
-            return caches.match(evt.request);
-        })
+
+function isSuccessful(response) {
+    return response &&
+        response.status === 200 &&
+        response.type === 'basic';
+}
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                if (response) {
+                    return response; // Cache hit
+                }
+                return fetch(event.request.clone())
+                    .then(function (response) {
+                        if (!isSuccessful(response)) {
+                            return response;
+                        }
+                        caches.open(CACHE_NAME)
+                            .then(function (cache) {
+                                cache.put(event.request, response.clone());
+                            });
+                        return response;
+                    }
+                    );
+            })
     );
 });
